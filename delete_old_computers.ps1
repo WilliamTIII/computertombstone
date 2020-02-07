2400dc54d85a3451de45f3dba39fffce13f76e5f
WilliamTIII <41160954+WilliamTIII@users.noreply.github.com>
2020-02-07 10:34:12 -0700

    added delete_old_computers.ps1

---
+++ delete_old_computers.ps1
@@ -0,0 +1,17 @@
+﻿$computers=get-adcomputer -Filter * -Properties * | select @{Label="Name";Expression={$_.name}}, @{Label="OS";Expression={$_.operatingsystem}}, @{Label="Distinguishedname";Expression={$_.'distinguishedname'}}, @{Label="Password";Expression={$_.'ms-Mcs-AdmPwd'}}, @{Label="PassLastSet";Expression={$_.PasswordLastSet}}, @{Label="PassExpired";Expression={$_.'PasswordExpired'}}, @{Label="PassRequired";Expression={$_.'PasswordNotRequired'}}, @{Label="Modified";Expression={$_.'Modified'}}, @{Label="LogonCount";Expression={$_.'logonCount'}}, @{Label="LastLogonDate";Expression={$_.'LastLogonDate'}} 
+
+$Results=@()
+foreach($computer in $computers){
+    #pause
+    if($computer.lastlogondate -le ((get-date).adddays(-270))){
+        #Move-ADObject -Identity $computer.distinguishedname -TargetPath "OU=Computers,OU=Terminations,DC=SHAMROCKFOODS,DC=com" -WhatIf
+        $results+=$computer.name
+    }
+}
+$results.count
+
+
+
+$delete = $computers | ?{ $_.distinguishedname -notlike "*OU=cluster*" -and $_.os -like "*windows*" -and $_.lastlogondate -le ((get-date).adddays(-365)) } 
+
+
